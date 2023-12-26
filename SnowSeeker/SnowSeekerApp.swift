e9bf5359b6fcce72dd5f949cde5383c95a13bf26
Mehdi Chikhi <ci_mi@outlook.fr>
2023-12-26 09:34:53 +0100

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Mehdi on 26/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
